90e6a74cc514f2e39a60653206f674f86a4afd8c
Phi <aiwavision@protonmail.com>
2021-11-14 23:09:36 -0300

    format and commented cout

--- include/collisions.h
+++ include/collisions.h
@@ -3,41 +3,43 @@
 #include <cmath>
 #include <iostream>
 
+// A escala não funciona do jeito que eu esperava (ela aumenta "pra cada lado"), e mudar a divisão para só um dos cubos resolve isso
+// Admito que não sei explicar porque isso resolve
 bool cubeToCubeCollision(cube &cube1, cube &cube2)
 {
-    if (fabs(cube1.posx - cube2.posx) < (cube1.scalex/2 + cube2.scalex))
+    if (fabs(cube1.posx - cube2.posx) < (cube1.scalex / 2 + cube2.scalex))
     {
-        
+        /*
         std::cout << "X: "
                   << fabs(cube1.posx - cube2.posx)
                   << " < "
-                  << ( cube1.scalex/2 +  cube2.scalex)
+                  << (cube1.scalex / 2 + cube2.scalex)
                   << " = "
-                  << (fabs(cube1.posx - cube2.posx) < ( cube1.scalex/2 +  cube2.scalex))
+                  << (fabs(cube1.posx - cube2.posx) < (cube1.scalex / 2 + cube2.scalex))
                   << "\n";
-        
-        if (fabs(cube1.posy - cube2.posy) < ( cube1.scaley/2 +  cube2.scaley))
+        */
+        if (fabs(cube1.posy - cube2.posy) < (cube1.scaley / 2 + cube2.scaley))
         {
-            
+            /*
             std::cout << "Y: "
                       << fabs(cube1.posy - cube2.posy)
                       << " < "
-                      << ( cube1.scaley/2 +  cube2.scaley)
+                      << (cube1.scaley / 2 + cube2.scaley)
                       << " = "
-                      << (fabs(cube1.posy - cube2.posy) < ( cube1.scaley/2 +  cube2.scaley))
+                      << (fabs(cube1.posy - cube2.posy) < (cube1.scaley / 2 + cube2.scaley))
                       << "\n";
-            
-            if (fabs(cube1.posz - cube2.posz) < ( cube1.scalez/2 +  cube2.scalez))
+            */
+            if (fabs(cube1.posz - cube2.posz) < (cube1.scalez / 2 + cube2.scalez))
             {
-                
+                /*
                 std::cout << "Z: "
                           << fabs(cube1.posz - cube2.posz)
                           << " < "
-                          << ( cube1.scalez/2 +  cube2.scalez)
+                          << (cube1.scalez / 2 + cube2.scalez)
                           << " = "
-                          << (fabs(cube1.posz - cube2.posz) < ( cube1.scalez/2 +  cube2.scalez))
+                          << (fabs(cube1.posz - cube2.posz) < (cube1.scalez / 2 + cube2.scalez))
                           << "\n";
-                
+                */
                 return true;
             }
         }
