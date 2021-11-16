0e86b5803ee8f2acc881e56c9c9298ffba1d39a1
Phi <aiwavision@protonmail.com>
2021-11-16 10:47:13 -0300

    separated collisions into cpp

--- include/cube.h
+++ include/cube.h
@@ -15,31 +15,24 @@ struct cube
     float scaley{1};
     float scalez{1};
     ObjModel cubemodel;
-    cube(const char *path): cubemodel(path){};
+    cube(const char *path): cubemodel(path) {};
 
-    glm::mat4 getModel();
+    glm::mat4 getModel()
+    {
+        return Matrix_Identity() * Matrix_Translate(posx, posy, posz) * Matrix_Scale(scalex, scaley, scalez);
+    }
 
-    void setPos(float x, float y, float z);
+    void setPos(float x, float y, float z)
+    {
+        posx = x;
+        posy = y;
+        posz = z;
+    }
 
-    void setScale(float x, float y, float z);
+    void setScale(float x, float y, float z)
+    {
+        scalex = x;
+        scaley = y;
+        scalez = z;
+    }
 };
-
-
-glm::mat4 cube::getModel()
-{
-    return Matrix_Identity() * Matrix_Translate(posx, posy, posz) * Matrix_Scale(scalex, scaley, scalez);
-}
-
-void cube::setScale(float x, float y, float z)
-{
-    scalex = x;
-    scaley = y;
-    scalez = z;
-}
-
-void cube::setPos(float x, float y, float z)
-{
-    posx = x;
-    posy = y;
-    posz = z;
-}
