6c3f4dc916fe67c75432e39f99be863a00781a3e
Bruno Bastos <bruno.bastos96@hotmail.com>
2021-11-17 15:15:58 -0300

    implementando a camera lookat

--- src/main.cpp
+++ src/main.cpp
@@ -178,6 +178,12 @@ bool g_UsePerspectiveProjection = true;
 // Variável que controla se o texto informativo será mostrado na tela.
 bool g_ShowInfoText = true;
 
+// Variável que controla se a câmera lookat está ligada, se não estiver a câmera view que estará
+bool g_UseCameraLookat = false;
+
+// Variável que controla os botões de movimentação e tiro
+bool g_EnableButtonsForPlay = true;
+
 plane g_Wall = plane("../../data/plane.obj");
 
 // Variáveis que definem um programa de GPU (shaders). Veja função LoadShadersFromFiles().
@@ -304,6 +310,10 @@ int main(int argc, char *argv[])
     ComputeNormals(&cubemodel1.cubemodel);
     BuildTrianglesAndAddToVirtualScene(&cubemodel1.cubemodel);
 
+    cube targetmodel();
+    ComputeNormals(&hudmodel);
+    BuildTrianglesAndAddToVirtualScene(&hudmodel);
+
     g_Cubes[0].setPos(3, 0, -3);
     g_Cubes[0].setScale(3, 1, 1);
     g_Cubes[1].setPos(3, 0, 3);
@@ -346,6 +356,7 @@ int main(int argc, char *argv[])
         cos(g_CameraPhi) * cos(g_CameraTheta),
         0.0f);
     g_LastCameraPos = camera_c_point;
+
     //wall.loadModel("../../data/plane.obj");
     // Ficamos em loop, renderizando, até que o usuário feche a janela
     while (!glfwWindowShouldClose(window))
@@ -375,9 +386,26 @@ int main(int argc, char *argv[])
         // float r = g_CameraDistance;
 
         // Definição de movimento de câmera
-        changeCameraPos(camera_c_point, camera_view_vector);
-        changeCameraView(camera_view_vector);
         glm::vec4 camera_up_vector = glm::vec4(0.0f, 1.0f, 0.0f, 0.0f); // Vetor "up" fixado para apontar para o "céu" (eito Y global)
+        glm::vec4 camera_lookat_l = glm::vec4(0.0f,0.0f,0.0f,1.0f); // Ponto "l", para ibde a câmera estará olhando.
+
+        if (g_UseCameraLookat)
+        {
+            // Ativa a camera lookat
+            camera_view_vector = camera_lookat_l - camera_c_point;
+            changeCameraPos(camera_c_point, camera_view_vector);
+            changeCameraView(camera_view_vector);
+        }
+        else
+        {
+            camera_view_vector = glm::vec4(
+            cos(g_CameraPhi) * sin(g_CameraTheta),
+            -sin(g_CameraPhi),
+            cos(g_CameraPhi) * cos(g_CameraTheta),
+            0.0f);
+            changeCameraPos(camera_c_point, camera_view_vector);
+            changeCameraView(camera_view_vector);
+        }
 
         g_Player.posx = camera_c_point.x;
         g_Player.posy = camera_c_point.y;
@@ -453,38 +481,38 @@ int main(int argc, char *argv[])
             DrawVirtualObject("Cube");
         }
 
-
-
         // Render do rifle acima de todos os layers (exceto possível futuro HUD)
+        if (!g_UseCameraLookat)
+        {
+            model = Matrix_Scale(0.5f, 0.5f, 0.5f) * Matrix_Translate(0.45f, 0.0f, 0.0f) * Matrix_Rotate_X(M_PI) * Matrix_Rotate_Z(M_PI) * Matrix_Translate(0.0f, -0.35f, 0.85f);
+            glDisable(GL_DEPTH_TEST);
+            glUniformMatrix4fv(view_uniform, 1, GL_FALSE, glm::value_ptr(Matrix_Identity()));
+            glUniformMatrix4fv(projection_uniform, 1, GL_FALSE, glm::value_ptr(projection));
+            glUniformMatrix4fv(model_uniform, 1, GL_FALSE, glm::value_ptr(model));
+            glUniform1i(object_id_uniform, RIFLE);
+            DrawVirtualObject("rifle");
+            glEnable(GL_DEPTH_TEST);
 
-        model = Matrix_Scale(0.5f, 0.5f, 0.5f) * Matrix_Translate(0.45f, 0.0f, 0.0f) * Matrix_Rotate_X(M_PI) * Matrix_Rotate_Z(M_PI) * Matrix_Translate(0.0f, -0.35f, 0.85f);
-        glDisable(GL_DEPTH_TEST);
-        glUniformMatrix4fv(view_uniform, 1, GL_FALSE, glm::value_ptr(Matrix_Identity()));
-        glUniformMatrix4fv(projection_uniform, 1, GL_FALSE, glm::value_ptr(projection));
-        glUniformMatrix4fv(model_uniform, 1, GL_FALSE, glm::value_ptr(model));
-        glUniform1i(object_id_uniform, RIFLE);
-        DrawVirtualObject("rifle");
-        glEnable(GL_DEPTH_TEST);
+            // Desenhando o HUD
 
-        // Desenhando o HUD
-        
-        glDisable(GL_DEPTH_TEST);
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        glUniformMatrix4fv(view_uniform, 1, GL_FALSE, glm::value_ptr(Matrix_Identity()));
-        glUniformMatrix4fv(projection_uniform, 1, GL_FALSE, glm::value_ptr(Matrix_Identity()));      
+            glDisable(GL_DEPTH_TEST);
+            glEnable(GL_BLEND);
+            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+            glUniformMatrix4fv(view_uniform, 1, GL_FALSE, glm::value_ptr(Matrix_Identity()));
+            glUniformMatrix4fv(projection_uniform, 1, GL_FALSE, glm::value_ptr(Matrix_Identity()));
 
-        // O crosshair terá altura igual a 10% da altura da tela, e será
-        // "quadrado" (pois tem origem em uma imagem de textura quadrada)
-        
-        float crosshair_height_in_NDC = 0.1;
-        float crosshair_width_in_NDC  = crosshair_height_in_NDC / g_ScreenRatio;
-        model = Matrix_Scale( crosshair_width_in_NDC, crosshair_height_in_NDC, 1.0f );
-        glUniformMatrix4fv(model_uniform, 1, GL_FALSE, glm::value_ptr(model));
+            // O crosshair terá altura igual a 10% da altura da tela, e será
+            // "quadrado" (pois tem origem em uma imagem de textura quadrada)
 
-        glUniform1i(object_id_uniform, HUD);
-        DrawVirtualObject("hud");
-        glEnable(GL_DEPTH_TEST);
+            float crosshair_height_in_NDC = 0.1;
+            float crosshair_width_in_NDC  = crosshair_height_in_NDC / g_ScreenRatio;
+            model = Matrix_Scale( crosshair_width_in_NDC, crosshair_height_in_NDC, 1.0f );
+            glUniformMatrix4fv(model_uniform, 1, GL_FALSE, glm::value_ptr(model));
+
+            glUniform1i(object_id_uniform, HUD);
+            DrawVirtualObject("hud");
+            glEnable(GL_DEPTH_TEST);
+        }
 
         // Pegamos um vértice com coordenadas de modelo (0.5, 0.5, 0.5, 1) e o
         // passamos por todos os sistemas de coordenadas armazenados nas
@@ -1071,54 +1099,25 @@ double g_LastCursorPosX, g_LastCursorPosY;
 // Função callback chamada sempre que o usuário aperta algum dos botões do mouse
 void MouseButtonCallback(GLFWwindow *window, int button, int action, int mods)
 {
-    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    if(g_UseCameraLookat)
     {
-        // Se o usuário pressionou o botão esquerdo do mouse, guardamos a
-        // posição atual do cursor nas variáveis g_LastCursorPosX e
-        // g_LastCursorPosY.  Também, setamos a variável
-        // g_LeftMouseButtonPressed como true, para saber que o usuário está
-        // com o botão esquerdo pressionado.
-        glfwGetCursorPos(window, &g_LastCursorPosX, &g_LastCursorPosY);
-        g_LeftMouseButtonPressed = true;
-    }
-    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
-    {
+        if (button = GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+        {
+            glfwGetCursorPos(window, &g_LastCursorPosX, &g_LastCursorPosY);
+            g_LeftMouseButtonPressed = true;
+        }
+        if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
+        {
         // Quando o usuário soltar o botão esquerdo do mouse, atualizamos a
         // variável abaixo para false.
         g_LeftMouseButtonPressed = false;
+        }
     }
-    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+    else
     {
-        // Se o usuário pressionou o botão esquerdo do mouse, guardamos a
-        // posição atual do cursor nas variáveis g_LastCursorPosX e
-        // g_LastCursorPosY.  Também, setamos a variável
-        // g_RightMouseButtonPressed como true, para saber que o usuário está
-        // com o botão esquerdo pressionado.
-        glfwGetCursorPos(window, &g_LastCursorPosX, &g_LastCursorPosY);
-        g_RightMouseButtonPressed = true;
+
     }
-    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_RELEASE)
-    {
-        // Quando o usuário soltar o botão esquerdo do mouse, atualizamos a
-        // variável abaixo para false.
-        g_RightMouseButtonPressed = false;
-    }
-    if (button == GLFW_MOUSE_BUTTON_MIDDLE && action == GLFW_PRESS)
-    {
-        // Se o usuário pressionou o botão esquerdo do mouse, guardamos a
-        // posição atual do cursor nas variáveis g_LastCursorPosX e
-        // g_LastCursorPosY.  Também, setamos a variável
-        // g_MiddleMouseButtonPressed como true, para saber que o usuário está
-        // com o botão esquerdo pressionado.
-        glfwGetCursorPos(window, &g_LastCursorPosX, &g_LastCursorPosY);
-        g_MiddleMouseButtonPressed = true;
-    }
-    if (button == GLFW_MOUSE_BUTTON_MIDDLE && action == GLFW_RELEASE)
-    {
-        // Quando o usuário soltar o botão esquerdo do mouse, atualizamos a
-        // variável abaixo para false.
-        g_MiddleMouseButtonPressed = false;
-    }
+
 }
 
 // Função callback chamada sempre que o usuário movimentar o cursor do mouse em
@@ -1130,86 +1129,117 @@ void CursorPosCallback(GLFWwindow *window, double xpos, double ypos)
     // instante de tempo, e usamos esta movimentação para atualizar os
     // parâmetros que definem a posição da câmera dentro da cena virtual.
     // Assim, temos que o usuário consegue controlar a câmera.
-    constexpr auto speed = 0.005f;
-    // Deslocamento do cursor do mouse em x e y de coordenadas de tela!
-    float dx = xpos - g_LastCursorPosX;
-    float dy = ypos - g_LastCursorPosY;
 
-    // Evitando "pulos" da câmera quando o mouse fica fora da tela
-    if (dx > 10.0f || dy > 10.0f)
+    if (g_UseCameraLookat)
     {
+        if (!g_LeftMouseButtonPressed)
+            return;
+
+        float dx = xpos - g_LastCursorPosX;
+        float dy = ypos - g_LastCursorPosY;
+
+        g_CameraTheta -= 0.01f*dx;
+        g_CameraPhi   += 0.01f*dy;
+
+        float phimax = 3.141592f/2;
+        float phimin = -phimax;
+
+        if (g_CameraPhi > phimax)
+            g_CameraPhi = phimax;
+
+        if (g_CameraPhi < phimin)
+            g_CameraPhi = phimin;
+
         g_LastCursorPosX = xpos;
         g_LastCursorPosY = ypos;
-        return;
     }
-
-    // Atualizamos parâmetros da câmera com os deslocamentos
-    g_CameraTheta -= speed * dx;
-    g_CameraPhi += speed * dy;
-
-    // Em coordenadas esféricas, o ângulo phi deve ficar entre -pi/2 e +pi/2.
-    float phimax = 3.141592f / 2;
-    float phimin = -phimax;
-
-    if (g_CameraPhi > phimax)
-        g_CameraPhi = phimax;
-
-    if (g_CameraPhi < phimin)
-        g_CameraPhi = phimin;
-
-    // Atualizamos as variáveis globais para armazenar a posição atual do
-    // cursor como sendo a última posição conhecida do cursor.
-    g_LastCursorPosX = xpos;
-    g_LastCursorPosY = ypos;
-
-    if (g_RightMouseButtonPressed)
+    else
     {
+
+        constexpr auto speed = 0.005f;
         // Deslocamento do cursor do mouse em x e y de coordenadas de tela!
         float dx = xpos - g_LastCursorPosX;
         float dy = ypos - g_LastCursorPosY;
 
-        // Atualizamos parâmetros da antebraço com os deslocamentos
-        g_ForearmAngleZ -= 0.01f * dx;
-        g_ForearmAngleX += 0.01f * dy;
+        // Evitando "pulos" da câmera quando o mouse fica fora da tela
+        if (dx > 10.0f || dy > 10.0f)
+        {
+            g_LastCursorPosX = xpos;
+            g_LastCursorPosY = ypos;
+            return;
+        }
+
+        // Atualizamos parâmetros da câmera com os deslocamentos
+        g_CameraTheta -= speed * dx;
+        g_CameraPhi += speed * dy;
+
+        // Em coordenadas esféricas, o ângulo phi deve ficar entre -pi/2 e +pi/2.
+        float phimax = 3.141592f / 2;
+        float phimin = -phimax;
+
+        if (g_CameraPhi > phimax)
+            g_CameraPhi = phimax;
+
+        if (g_CameraPhi < phimin)
+            g_CameraPhi = phimin;
 
         // Atualizamos as variáveis globais para armazenar a posição atual do
         // cursor como sendo a última posição conhecida do cursor.
         g_LastCursorPosX = xpos;
         g_LastCursorPosY = ypos;
-    }
 
-    if (g_MiddleMouseButtonPressed)
-    {
-        // Deslocamento do cursor do mouse em x e y de coordenadas de tela!
-        float dx = xpos - g_LastCursorPosX;
-        float dy = ypos - g_LastCursorPosY;
+        if (g_RightMouseButtonPressed)
+        {
+            // Deslocamento do cursor do mouse em x e y de coordenadas de tela!
+            float dx = xpos - g_LastCursorPosX;
+            float dy = ypos - g_LastCursorPosY;
 
-        // Atualizamos parâmetros da antebraço com os deslocamentos
-        g_TorsoPositionX += 0.01f * dx;
-        g_TorsoPositionY -= 0.01f * dy;
+            // Atualizamos parâmetros da antebraço com os deslocamentos
+            g_ForearmAngleZ -= 0.01f * dx;
+            g_ForearmAngleX += 0.01f * dy;
 
-        // Atualizamos as variáveis globais para armazenar a posição atual do
-        // cursor como sendo a última posição conhecida do cursor.
-        g_LastCursorPosX = xpos;
-        g_LastCursorPosY = ypos;
+            // Atualizamos as variáveis globais para armazenar a posição atual do
+            // cursor como sendo a última posição conhecida do cursor.
+            g_LastCursorPosX = xpos;
+            g_LastCursorPosY = ypos;
+        }
+
+        if (g_MiddleMouseButtonPressed)
+        {
+            // Deslocamento do cursor do mouse em x e y de coordenadas de tela!
+            float dx = xpos - g_LastCursorPosX;
+            float dy = ypos - g_LastCursorPosY;
+
+            // Atualizamos parâmetros da antebraço com os deslocamentos
+            g_TorsoPositionX += 0.01f * dx;
+            g_TorsoPositionY -= 0.01f * dy;
+
+            // Atualizamos as variáveis globais para armazenar a posição atual do
+            // cursor como sendo a última posição conhecida do cursor.
+            g_LastCursorPosX = xpos;
+            g_LastCursorPosY = ypos;
+        }
     }
 }
 
 // Função callback chamada sempre que o usuário movimenta a "rodinha" do mouse.
 void ScrollCallback(GLFWwindow *window, double xoffset, double yoffset)
 {
-    // Atualizamos a distância da câmera para a origem utilizando a
-    // movimentação da "rodinha", simulando um ZOOM.
-    g_CameraDistance -= 0.1f * yoffset;
+    if (g_UseCameraLookat)
+    {
+        // Atualizamos a distância da câmera para a origem utilizando a
+        // movimentação da "rodinha", simulando um ZOOM.
+        g_CameraDistance -= 0.1f * yoffset;
 
-    // Uma câmera look-at nunca pode estar exatamente "em cima" do ponto para
-    // onde ela está olhando, pois isto gera problemas de divisão por zero na
-    // definição do sistema de coordenadas da câmera. Isto é, a variável abaixo
-    // nunca pode ser zero. Versões anteriores deste código possuíam este bug,
-    // o qual foi detectado pelo aluno Vinicius Fraga (2017/2).
-    const float verysmallnumber = std::numeric_limits<float>::epsilon();
-    if (g_CameraDistance < verysmallnumber)
-        g_CameraDistance = verysmallnumber;
+        // Uma câmera look-at nunca pode estar exatamente "em cima" do ponto para
+        // onde ela está olhando, pois isto gera problemas de divisão por zero na
+        // definição do sistema de coordenadas da câmera. Isto é, a variável abaixo
+        // nunca pode ser zero. Versões anteriores deste código possuíam este bug,
+        // o qual foi detectado pelo aluno Vinicius Fraga (2017/2).
+        const float verysmallnumber = std::numeric_limits<float>::epsilon();
+        if (g_CameraDistance < verysmallnumber)
+            g_CameraDistance = verysmallnumber;
+    }
 }
 
 // Definição da função que será chamada sempre que o usuário pressionar alguma
@@ -1275,6 +1305,22 @@ void KeyCallback(GLFWwindow *window, int key, int scancode, int action, int mod)
     {
         g_UsePerspectiveProjection = false;
     }
+
+    // Se o usuário apertar a tecla L, utilizamos a câmera Lookat.
+    if (key == GLFW_KEY_L && action == GLFW_PRESS)
+    {
+        g_UseCameraLookat = true;
+        g_EnableButtonsForPlay = false;
+    }
+
+    // Se o usuário apertar a tecla V, utilizamos a câmera View.
+    if (key == GLFW_KEY_V && action == GLFW_PRESS)
+    {
+        g_UseCameraLookat = false;
+        g_EnableButtonsForPlay = true;
+    }
+
+
 
     // Se o usuário apertar a tecla H, fazemos um "toggle" do texto informativo mostrado na tela.
     if (key == GLFW_KEY_H && action == GLFW_PRESS)
@@ -1648,42 +1694,44 @@ void changeCameraPos(glm::vec4 &c_point, const glm::vec4 &view_vector)
     glm::vec4 up_vector = glm::vec4(0.0f, 1.0f, 0.0f, 0.0f);
     auto w = -view_vector / norm(view_vector);
     auto u = crossproduct(up_vector, w) / norm(crossproduct(up_vector, w));
-
-    if (g_PressedKeys["W"])
+    if(g_EnableButtonsForPlay)
     {
-        calculate_pos.x -= w.x * speed;
-        calculate_pos.z -= w.z * speed;
-    }
-    if (g_PressedKeys["S"])
-    {
-        calculate_pos.x += w.x * speed;
-        calculate_pos.z += w.z * speed;
-    }
-    if (g_PressedKeys["D"])
-    {
-        calculate_pos.x += u.x * speed;
-        calculate_pos.z += u.z * speed;
-    }
-    if (g_PressedKeys["A"])
-    {
-        calculate_pos.x -= u.x * speed;
-        calculate_pos.z -= u.z * speed;
-    }
-    if (calculate_pos == g_LastCameraPos)
-        return;
-
-    g_Player.setPos(calculate_pos.x, calculate_pos.y, calculate_pos.z);
-    if (cylinderToPlaneCollision(g_Player, g_Wall))
-        return;
-    for (auto &&cube : g_Cubes)
-    {
-        if (cubeToCylinderCollision(cube, g_Player))
+        if (g_PressedKeys["W"])
         {
-            return;
+            calculate_pos.x -= w.x * speed;
+            calculate_pos.z -= w.z * speed;
         }
+        if (g_PressedKeys["S"])
+        {
+            calculate_pos.x += w.x * speed;
+            calculate_pos.z += w.z * speed;
+        }
+        if (g_PressedKeys["D"])
+        {
+            calculate_pos.x += u.x * speed;
+            calculate_pos.z += u.z * speed;
+        }
+        if (g_PressedKeys["A"])
+        {
+            calculate_pos.x -= u.x * speed;
+            calculate_pos.z -= u.z * speed;
+        }
+        if (calculate_pos == g_LastCameraPos)
+            return;
+
+        g_Player.setPos(calculate_pos.x, calculate_pos.y, calculate_pos.z);
+        if (cylinderToPlaneCollision(g_Player, g_Wall))
+            return;
+        for (auto &&cube : g_Cubes)
+        {
+            if (cubeToCylinderCollision(cube, g_Player))
+            {
+                return;
+            }
+        }
+        c_point = calculate_pos;
+        g_LastCameraPos = c_point;
     }
-    c_point = calculate_pos;
-    g_LastCameraPos = c_point;
 }
 
 void print_vec4(const glm::vec4 &v){
